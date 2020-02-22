45dbc2c6ffb3ef0fa1176bb1e72cf86b1ca974cc
viralsanghavi <vsanghavi3@gmail.com>
2020-02-22 14:40:32 +0530

    Initial Release

--- calculator.c
+++ calculator.c
@@ -4,7 +4,7 @@ void main()
 	int a,b,var;
 	printf("Enter the two numbers: \n");
 	scanf("%d,%d",&a,&b);
-	printf("\n Enter your choice \
+	printf("\n Enter your choices\
 		\n 1.Addition");
 	printf("\n Enter the choice now:");
 	scanf("%d",&var);
